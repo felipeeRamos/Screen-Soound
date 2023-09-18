27754549c402179a4c19a8d328c98c0cc757f506
Luis Felipe Ramos <116222857+felipeeRamos@users.noreply.github.com>
2023-09-17 23:21:58 -0300

    Exercicio POO

--- Screen Soound/Program.cs
+++ Screen Soound/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿Musica  musica1 = new Musica();
+musica1.nome = "Cry in The Club";
+musica1.artista = "Camila Cabello";
+
+Console.WriteLine($"Nome: {musica1.nome}");
+Console.WriteLine($"Artista: {musica1.artista}");
+
+Musica musica2 = new Musica();
+musica2.nome = "Downtown";
+musica2.artista = "Anitta";
+
+Console.WriteLine($"Nome: {musica2.nome}");
+Console.WriteLine($"Artista: {musica2.artista}");

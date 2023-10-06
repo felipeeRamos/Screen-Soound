50392a7742da8e89953266f012d020459a4a2a8a
Luis Felipe Ramos <116222857+felipeeRamos@users.noreply.github.com>
2023-10-05 21:51:04 -0300

    POO

---
+++ Screen Soound/Podcast.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Screen_Soound
+{
+    internal class Podcast
+    {
+    }
+}
+class Podcast
+{
+    public string Host { get; set; }
+    public string Nome { get; set; }
+    public int TotalEpsodios { get; set; }
